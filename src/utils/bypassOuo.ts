ee2c20c0502f01bccce831c3391ff1a2efeb103e
MoonLGH <MoonLMC1212@gmail.com>
2023-01-05 12:40:04 +0000

    more recrusive shit so you get data less faster lmao

--- src/utils/bypassOuo.ts
+++ src/utils/bypassOuo.ts
@@ -3,41 +3,45 @@ import { load } from "cheerio";
 import {Cookie, parse} from "set-cookie-parser"
 
 async function get(url:string):Promise<any> {
-    if (url.includes("/go/")) url = url.replace("/go/", "/");
-    if (url.includes("/fbc/")) url = url.replace("/fbc/", "/");
-
-    let header:any = {}
-    let resp = await axios.get(url);
-
-    let $ = load(resp.data);
-
-    let post = $("form[method='POST']").attr("action");
-    let tk = $("input[name='_token']").attr("value");
-    let cookie = cookieString(parse(resp.headers["set-cookie"]!));
-    let body = `_token=${tk}&x-token=&v-token=bx`;
-
-    header["Cookie"] = cookie;
-    header["Content-Type"] = "application/x-www-form-urlencoded";
-    header["Content-Length"] = byteCount(body);
-    header["Referer"] = url;
-    header["Sec-Fetch-Dest"] = "document";
-    header["Sec-Fetch-Mode"] = "navigate";
-    header["Sec-Fetch-Site"] = "same-origin";
-    header["TE"] = "trailers";
-
     try {
-        const resp = await axios({
-            method: 'POST',
-            url: post!.replace('/go', '/xreallcygo'),
-            data: body,
-            headers: header,
-            maxRedirects: 0,
-          });
-        if (resp.data) {
-            return (await get(url));
+        if (url.includes("/go/")) url = url.replace("/go/", "/");
+        if (url.includes("/fbc/")) url = url.replace("/fbc/", "/");
+    
+        let header:any = {}
+        let resp = await axios.get(url);
+    
+        let $ = load(resp.data);
+    
+        let post = $("form[method='POST']").attr("action");
+        let tk = $("input[name='_token']").attr("value");
+        let cookie = cookieString(parse(resp.headers["set-cookie"]!));
+        let body = `_token=${tk}&x-token=&v-token=bx`;
+    
+        header["Cookie"] = cookie;
+        header["Content-Type"] = "application/x-www-form-urlencoded";
+        header["Content-Length"] = byteCount(body);
+        header["Referer"] = url;
+        header["Sec-Fetch-Dest"] = "document";
+        header["Sec-Fetch-Mode"] = "navigate";
+        header["Sec-Fetch-Site"] = "same-origin";
+        header["TE"] = "trailers";
+    
+        try {
+            const resp = await axios({
+                method: 'POST',
+                url: post!.replace('/go', '/xreallcygo'),
+                data: body,
+                headers: header,
+                maxRedirects: 0,
+              });
+            if (resp.data) {
+                return (await get(url));
+            }
+        } catch (err:any) {
+            if (err.response?.headers?.location) return err.response.headers.location;
         }
-    } catch (err:any) {
-        if (err.response?.headers?.location) return err.response.headers.location;
+    } catch (err) {
+        return (await get(url))
     }
 }
 function cookieString(co:Cookie[]) {
